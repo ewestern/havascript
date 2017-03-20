e5bb7cabf2005e5fcb5af9496ffc718a829d4947
Peter France <peter.france@moat.com>
2017-03-20 08:42:35 -0400

    init codegen

---
+++ CodeGen/CodeGen.hs
@@ -0,0 +1,39 @@
+module CodeGen.CodeGen where
+
+import qualified Data.Map as Map
+import Data.Word
+
+import Control.Monad.State
+import Control.Applicative
+
+import LLVM.General.AST
+import LLVM.General.AST.Global
+import qualified LLVM.General.AST as AST
+
+import qualified LLVM.General.AST.Constant as C
+import qualified LLVM.General.AST.Attribute as A
+import qualified LLVM.General.AST.CallingConvention as CC
+import qualified LLVM.General.AST.FloatingPointPredicate as FP
+
+
+type SymbolTable = [(String, Operand)]
+
+data CodegenState
+  = CodegenState {
+    currentBlock :: Name                     -- Name of the active block to append to
+  , blocks       :: Map.Map Name BlockState  -- Blocks for function
+  , symtab       :: SymbolTable              -- Function scope symbol table
+  , blockCount   :: Int                      -- Count of basic blocks
+  , count        :: Word                     -- Count of unnamed instructions
+  , names        :: Names                    -- Name Supply
+  } deriving Show
+
+data BlockState
+  = BlockState {
+    idx   :: Int                            -- Block index
+  , stack :: [Named Instruction]            -- Stack of instructions
+  , term  :: Maybe (Named Terminator)       -- Block terminator
+  } deriving Show
+
+newtype Codegen a = Codegen { runCodegen :: State CodegenState a }
+  deriving (Functor, Applicative, Monad, MonadState CodegenState )
